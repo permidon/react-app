4fa458ebb2a8ddb171e21c29e69c3be655777e63
Roman Perminov <permidon@me.com>
2018-01-11 19:54:47 -0500

    ssr wip

--- initializers/server/index.js
+++ initializers/server/index.js
@@ -1,22 +1,44 @@
 /* eslint-disable */
+const path = require('path');
+require('app-module-path').addPath(path.join(process.cwd(), 'src'));
 
-const webpack = require('webpack');
-const webpackDevServer = require('webpack-dev-server');
+require('./globals');
 
-const config = require('../../webpack.config.js');
+require('babel-core/register');
+require.extensions['.css'] = () => {
+  return;
+};
 
-const host = 'localhost';
+const express = require('express');
+const application = express();
 const port = 3000;
 
-new webpackDevServer(webpack(config), {
-  hot: true,
-  historyApiFallback: true,
-  publicPath: config.output.publicPath,
-  stats: { color: true }
-}).listen(port, host, (err) => {
-  if (err) {
-    console.log(err);
-  }
+application.set('views', __dirname);
+application.set('view engine', 'ejs');
 
-  console.log(`Listening ${host} on ${port}`);
+const webpack = require('webpack');
+const config = require('../../webpack.config').default;
+const compiler = webpack(config);
+const webpackDev = require('webpack-dev-middleware');
+const webpackHot = require('webpack-hot-middleware');
+
+application.use(express.static('src/static'));
+
+application.use(
+  webpackDev(
+    compiler,
+    {
+      hot: true,
+      publicPath: config.output.publicPath,
+      stats: { colors: true }
+    }
+  )
+);
+
+application.use(webpackHot(compiler));
+
+application.get('*', require('./render').default);
+
+application.listen(port, function() {
+  console.log(`Server on ${port}`);
 });
